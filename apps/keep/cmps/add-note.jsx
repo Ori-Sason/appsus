7418353470c47fdd4f401b90d4096c3e450a3aef
Ori Sason <orisason1@gmail.com>
2022-04-27 14:03:01 +0300

    keep filter

---
+++ apps/keep/cmps/add-note.jsx
@@ -0,0 +1,8 @@
+export function AddNote() {
+    return <section className="add-note">
+        <p>Take a note...</p>
+        <button className="img-checkbox-checked clean-btn"></button>
+        <button className="img-img-btn clean-btn"></button>
+        <button className="img-youtube clean-btn"></button>
+    </section>
+}
