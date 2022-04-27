b949c4aeceee6665bdaea6ae0485f960a00fb376
Ori Sason <orisason1@gmail.com>
2022-04-27 10:44:36 +0300

    changed apps files names

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,10 +1,10 @@
 import { AppHeader } from './cmps/app-header.jsx'
 import { About } from './pages/app-about.jsx'
 import { Home } from './pages/app-home.jsx'
-import { BookApp } from './apps/book/pages/book-index.jsx'
+import { BookApp } from './apps/book/pages/app-book.jsx'
 import { BookDetails } from './apps/book/pages/book-details.jsx'
-import { KeepApp } from './apps/keep/pages/keep-index.jsx'
-import { MailApp } from './apps/mail/pages/mail-index.jsx'
+import { KeepApp } from './apps/keep/pages/app-keep.jsx'
+import { MailApp } from './apps/mail/pages/app-mail.jsx'
 import { UserMsg } from './cmps/user-msg.jsx'
 
 const Router = ReactRouterDOM.HashRouter
