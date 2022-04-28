06ccd9dce6a0e198bb2ce6405e8d046471cd77f0
Ori Sason <orisason1@gmail.com>
2022-04-28 20:34:22 +0300

    note-list to function

--- apps/keep/cmps/notes-list.jsx
+++ apps/keep/cmps/notes-list.jsx
@@ -1,12 +1,8 @@
-import { notesService } from '../services/notes.service.js'
 import { NotePreview } from './notes-preview.jsx'
 
-export class NoteList extends React.Component {
-    render() {
-        const { notes } = this.props
-        if (!notes) return <p>Loading...</p>
-        return <section className="notes-list">
-            {notes.map(note => <NotePreview key={note.id} note={note} onUpdate={this.props.onUpdate} />)}
-        </section>
-    }
+export function NoteList({ notes, onUpdate }) {
+    if (!notes) return <p>Loading...</p>
+    return <section className="notes-list">
+        {notes.map(note => <NotePreview key={note.id} note={note} onUpdate={onUpdate} />)}
+    </section>
 }
