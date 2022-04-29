9220d9df03a941fafa50ffe00d69a3424fcf246f
Ori Sason <orisason1@gmail.com>
2022-04-29 18:34:16 +0300

    notes - added mail button UI and some responsive fixes

--- apps/keep/cmps/note-btns.jsx
+++ apps/keep/cmps/note-btns.jsx
@@ -2,10 +2,13 @@ export function NoteBtns({ isPreview, isCreate, onUpdate, onClose, onDelete, onD
     return <section className="note-btns">
         <button type="button" className="note-btn img-reminder clean-btn" onClick={(ev) => onReminder(ev, noteId)}></button>
         <button type="button" className="note-btn img-color clean-btn"></button>
+        
         {!isCreate && <button type="button" className="note-btn img-archive clean-btn" onClick={(ev) => onArchive(ev, noteId)}></button>}
         {!isCreate && <button type="button" className="note-btn img-duplicate clean-btn" onClick={(ev) => onDuplicate(ev, noteId)}></button>}
+        {isPreview && <button type="button" className="note-btn img-mail clean-btn" onClick={(ev) => onDelete(ev, noteId)}></button>}
         {!isCreate && <button type="button" className="note-btn img-bin clean-btn" onClick={(ev) => onDelete(ev, noteId)}></button>}
-        <button type="button" className={`note-btn btn-text clean-btn ${isPreview ? 'hide' : ''}`} onClick={onClose}>Close</button>
-        <button type="submit" className={`note-btn btn-text clean-btn ${isPreview ? 'hide' : ''}`} onClick={onUpdate}>{isCreate ? 'Create' : 'Save'}</button>
+        
+        {!isPreview && <button type="button" className={`note-btn btn-text clean-btn`} onClick={onClose}>Close</button>}
+        {!isPreview && <button type="submit" className={`note-btn btn-text clean-btn`} onClick={onUpdate}>{isCreate ? 'Create' : 'Save'}</button>}
     </section>
 }
