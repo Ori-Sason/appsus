8780d7fdf54e61d7a8f8d247cf225aedd13221b7
AlexYa <1yakovlevalex@gmail.com>
2022-04-30 21:29:28 +0300

    on menu click black screen appears

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -22,13 +22,14 @@ export class AppHeader extends React.Component {
         </Link>
         <nav onClick={this.toggleMenu} className={`header-nav-links-btn ${isMenu?'active':''}`}>
           {isMenu&&<div className="header-menu-links">
+            
           <NavLink className='nav-home' to="/" exact><span className="nav-home-span">Home</span></NavLink>
           <NavLink className='nav-book' to="/book"><span className="nav-book-span">Books</span></NavLink>
           <NavLink className='nav-keep' to="/keep"><span className="nav-keep-span">Keep</span></NavLink>
           <NavLink className='nav-mail'to="/mail"><span className="nav-mail-span">Mail</span></NavLink>
           <NavLink className='nav-about'to="/about"><span className="nav-about-span">About</span></NavLink>
           </div>}
-        
+         {isMenu&&<div onClick={this.toggleMenu} className="background-screen-back"></div>}
         </nav>
       </header>
     </section>
