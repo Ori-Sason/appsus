3eddef8aee94af81da2933d25075a6fc5416d41f
Ori Sason <orisason1@gmail.com>
2022-05-01 21:55:06 +0300

    notes - makeid, changed reminder to date, toggleNote function, toggleUpdateProperty function

--- services/util.service.js
+++ services/util.service.js
@@ -1,9 +1,9 @@
 export const utilService = {
-    getCurrencySymbol,
-    makeId,
-    getRandomIntInclusive,
-    makeLorem,
-    formatAMPM
+  getCurrencySymbol,
+  makeId,
+  getRandomIntInclusive,
+  makeLorem,
+  formatAMPM
 }
 
 function getCurrencySymbol(num, currencyCode) {
@@ -13,13 +13,12 @@ function getCurrencySymbol(num, currencyCode) {
   }).format(num)
 }
 
-
 function makeLorem(size = 100) {
   var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
   var txt = ''
   while (size > 0) {
-      size--
-      txt += words[Math.floor(Math.random() * words.length)] + ' '
+    size--
+    txt += words[Math.floor(Math.random() * words.length)] + ' '
   }
   return txt
 }
@@ -30,25 +29,25 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
-
 function makeId(length = 6) {
   var txt = ''
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
   for (var i = 0; i < length; i++) {
-      txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
   }
 
   return txt
 }
-function formatAMPM (date) {
-  date= new Date(date)
+
+function formatAMPM(date) {
+  date = new Date(date)
   let hours = date.getHours();
-  let minutes = date.getMinutes();    
+  let minutes = date.getMinutes();
   const ampm = hours >= 12 ? 'pm' : 'am';
 
   hours %= 12;
-  hours = hours || 12;    
+  hours = hours || 12;
   minutes = minutes < 10 ? `0${minutes}` : minutes;
 
   const strTime = `${hours}:${minutes} ${ampm}`;
